494ebb87add466f77631fff8c3bd8b70dd45126e
ervinMamutov <ervin.news@gmail.com>
2023-08-10 01:33:17 +0200

    finish thrid iteration

--- src/components/createPokemonComponents.js
+++ src/components/createPokemonComponents.js
@@ -1,9 +1,12 @@
 const createPokemonComponents = (data) => {
   const container = document.createElement('div');
+  container.classList.add('pokemon-container');
+
   const pokemonImg = document.createElement('img');
   pokemonImg.classList.add('pokemon-img');
   const title = document.createElement('h4');
   const description = document.createElement('p');
+  description.classList.add('description');
 
   pokemonImg.src = data.sprites.other.home.front_default;
   console.log(pokemonImg.src);
